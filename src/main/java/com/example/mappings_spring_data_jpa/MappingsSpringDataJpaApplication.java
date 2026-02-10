91e87e9a5b32e0eacc88bdc9b07f886e90451c9e
theplaintextengineer <akshay2104.singh@gmail.com>
2026-02-10 12:06:06 +0000

    Inital Commit

---
+++ src/main/java/com/example/mappings_spring_data_jpa/MappingsSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mappings_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingsSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingsSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mappings_spring_data_jpa/MappingsSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mappings_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingsSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
